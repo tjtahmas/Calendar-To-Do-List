a039b96a5a40927af6b7623d07ee51ffed30d38b
Thomas Tahmassebi <tjtahmas@gmail.com>
2021-10-17 17:59:44 -0400

    stored text to local storage, still a littel buggy

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
 
 //current day is displayed at top (moment.js)
 
+renderText();
+
 function clock() {
     var today = moment();
     //today = today.add(14, 'h'); //Tester for different times of day
@@ -132,20 +134,157 @@ function getChars(event) { //Gets number of characters from textbox
     // First create a DIV element.
     var newP = document.createElement('li');
     var x = document.getElementById("InputBox").value;
-    console.log(x)
     newP.innerHTML = x;
     // newP.innerHTML = document.getElementById("InputBox").value;
     // console.log(document.getElementById("InputBox").)
     // console.log(newP.innerHTML);
     descrip.lastChild.remove();
-    myStorage = window.localStorage;
 
     descrip.appendChild(newP);
+    saveText();
     inputSwitch = false;
 
 }
 
 //text is saved in local storage
+
+function saveText() {
+    // Save related form data as an object
+    var time9 = document.getElementById('9').children;
+    var time10 = document.getElementById('10').children;
+    var time11 = document.getElementById('11').children;
+    var time12 = document.getElementById('12').children;
+    var time1 = document.getElementById('1').children;
+    var time2 = document.getElementById('2').children;
+    var time3 = document.getElementById('3').children;
+    var time4 = document.getElementById('4').children;
+    var time5 = document.getElementById('5').children;
+
+    var text9 = [''];
+    var text10 = [''];
+    var text11 = [''];
+    var text12 = [''];
+    var text1 = [''];
+    var text2 = [''];
+    var text3 = [''];
+    var text4 = [''];
+    var text5 = [''];
+
+    for (var i=0;i < time9.length;i++){
+        text9[i] = time9[i].textContent;
+    }
+    for (var i=0;i < time10.length;i++){
+        text10[i] = time10[i].textContent;
+    }
+    for (var i=0;i < time11.length;i++){
+        text11[i] = time11[i].textContent;
+    }
+    for (var i=0;i < time12.length;i++){
+        text12[i] = time12[i].textContent;
+    }
+    for (var i=0;i < time1.length;i++){
+        text1[i] = time1[i].textContent;
+    }
+    for (var i=0;i < time2.length;i++){
+        text2[i] = time2[i].textContent;
+    }
+    for (var i=0;i < time3.length;i++){
+        text3[i] = time3[i].textContent;
+    }
+    for (var i=0;i < time4.length;i++){
+        text4[i] = time4[i].textContent;
+    }
+    for (var i=0;i < time5.length;i++){
+        text5[i] = time5[i].textContent;
+    }
+
+    var textItems = {
+      element9: text9,
+      element10: text10,
+      element11: text11,
+      element12: text12,
+      element1: text1,
+      element2: text2,
+      element3: text3,
+      element4: text4,
+      element5: text5,
+    };
+
+    // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
+    localStorage.setItem("textItems", JSON.stringify(textItems));
+  }
+  
+function renderText() {
+    // Use JSON.parse() to convert text to JavaScript object
+    var textItems = JSON.parse(localStorage.getItem("textItems"));
+    // Check if data is returned, if not exit out of the function
+    if (textItems.element9 !== undefined) {
+        var descript9 = document.getElementById("9");
+        for (var i = 0; i < textItems.element9.length; i++) {
+            var newli = document.createElement('li');
+            newli.innerHTML = textItems.element9[i];
+            descript9.appendChild(newli);
+        }
+    } if (textItems.element10 !== undefined) {
+        var descript10 = document.getElementById("10");
+        for (var i = 0; i < textItems.element10.length; i++) {
+            var newli = document.createElement('li');
+            newli.innerHTML = textItems.element10[i];
+            descript10.appendChild(newli);
+        }
+    } if (textItems.element11 !== undefined) {
+        var descript11 = document.getElementById("11");
+        for (var i = 0; i < textItems.element11.length; i++) {
+            var newli = document.createElement('li');
+            newli.innerHTML = textItems.element11[i];
+            descript11.appendChild(newli);
+        }
+    } if (textItems.element12 !== undefined) {
+        var descript12 = document.getElementById("12");
+        for (var i = 0; i < textItems.element12.length; i++) {
+            var newli = document.createElement('li');
+            newli.innerHTML = textItems.element12[i];
+            descript12.appendChild(newli);
+        }
+    } if (textItems.element1 !== undefined) {
+        var descript1 = document.getElementById("1");
+        for (var i = 0; i < textItems.element1.length; i++) {
+            var newli = document.createElement('li');
+            newli.innerHTML = textItems.element1[i];
+            descript1.appendChild(newli);
+        }
+    } if (textItems.element2 !== undefined) {
+        var descript2 = document.getElementById("2");
+        for (var i = 0; i < textItems.element2.length; i++) {
+            var newli = document.createElement('li');
+            newli.innerHTML = textItems.element2[i];
+            descript2.appendChild(newli);
+        }
+    } if (textItems.element3 !== undefined) {
+        var descript3 = document.getElementById("3");
+        for (var i = 0; i < textItems.element3.length; i++) {
+            var newli = document.createElement('li');
+            newli.innerHTML = textItems.element3[i];
+            descript3.appendChild(newli);
+        }
+    } if (textItems.element4 !== undefined) {
+        var descript4 = document.getElementById("4");
+        for (var i = 0; i < textItems.element4.length; i++) {
+            var newli = document.createElement('li');
+            newli.innerHTML = textItems.element4[i];
+            descript4.appendChild(newli);
+        }
+    } if (textItems.element5 !== undefined) {
+        var descript5 = document.getElementById("5");
+        for (var i = 0; i < textItems.element5.length; i++) {
+            var newli = document.createElement('li');
+            newli.innerHTML = textItems.element5[i];
+            descript5.appendChild(newli);
+        }
+    } else {
+        return;
+    }
+}
 
 //refresh and the page persists
 
