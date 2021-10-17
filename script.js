b80b70593abef8ebaadc25bb0f0d9e08dabcc5b5
Thomas Tahmassebi <tjtahmas@gmail.com>
2021-10-16 22:41:51 -0400

    Got text input to work, need to work on local storage

--- script.js
+++ script.js
@@ -7,10 +7,10 @@
 
 function clock() {
     var today = moment();
-    today = today.add(14,'h'); //Tester for different times of day
+    today = today.add(14, 'h'); //Tester for different times of day
     //console.log(today);
     var currentTime = today.format('MMMM Do YYYY, h:mm:ss a');
-    $('#timer').text(currentTime) 
+    $('#timer').text(currentTime)
 
 
     //timeblocks color coded to indicate past,present,future (gray for past, red for present, green for future)
@@ -18,69 +18,69 @@ function clock() {
 
     var hour = today.format('h');
     var AMorPM = today.format('a');
-    if (hour == 9 && AMorPM == 'am'){ //9AM conditions
+    if (hour == 9 && AMorPM == 'am') { //9AM conditions
         $('#9AM').addClass("present");
-    } else if (hour < 9 && AMorPM == 'am'){
+    } else if (hour < 9 && AMorPM == 'am') {
         $('#9AM').addClass("future");
-    } else if (hour > 9 && AMorPM == 'am' || AMorPM == 'pm'){
+    } else if (hour > 9 && AMorPM == 'am' || AMorPM == 'pm') {
         $('#9AM').addClass("past");
     }
-    if (hour == 10 && AMorPM == 'am'){
+    if (hour == 10 && AMorPM == 'am') {
         $('#10AM').addClass("present");
-    } else if (hour < 10 && AMorPM == 'am'){
+    } else if (hour < 10 && AMorPM == 'am') {
         $('#10AM').addClass("future");
-    } else if (hour > 10 && AMorPM == 'am' || AMorPM == 'pm'){
+    } else if (hour > 10 && AMorPM == 'am' || AMorPM == 'pm') {
         $('#10AM').addClass("past");
     }
-    if (hour == 11 && AMorPM == 'am'){
+    if (hour == 11 && AMorPM == 'am') {
         $('#11AM').addClass("present");
-    } else if (hour < 11 && AMorPM == 'am'){
+    } else if (hour < 11 && AMorPM == 'am') {
         $('#11AM').addClass("future");
-    } else if (hour > 11 && AMorPM == 'am' || AMorPM == 'pm'){
+    } else if (hour > 11 && AMorPM == 'am' || AMorPM == 'pm') {
         $('#11AM').addClass("past");
     }
-    if (hour == 12 && AMorPM == 'pm'){
+    if (hour == 12 && AMorPM == 'pm') {
         $('#12PM').addClass("present");
-    } else if (hour < 12 && AMorPM == 'am'){
+    } else if (hour < 12 && AMorPM == 'am') {
         $('#12PM').addClass("future");
-    } else if (hour < 12 && AMorPM == 'pm'){
+    } else if (hour < 12 && AMorPM == 'pm') {
         $('#12PM').addClass("past");
     }
-    if (hour == 1 && AMorPM == 'pm'){
+    if (hour == 1 && AMorPM == 'pm') {
         $('#1PM').addClass("present");
-    } else if (hour == 12  && AMorPM == 'pm' || AMorPM == 'am'){
+    } else if (hour == 12 && AMorPM == 'pm' || AMorPM == 'am') {
         $('#1PM').addClass("future");
-    } else if (hour > 1 && AMorPM == 'pm'){
+    } else if (hour > 1 && AMorPM == 'pm') {
         $('#1PM').addClass("past");
     }
-    if (hour == 2 && AMorPM == 'pm'){
+    if (hour == 2 && AMorPM == 'pm') {
         $('#2PM').addClass("present");
-    } else if (hour == 12  && AMorPM == 'pm' || hour < 2 && AMorPM == 'pm' || AMorPM == 'am'){
+    } else if (hour == 12 && AMorPM == 'pm' || hour < 2 && AMorPM == 'pm' || AMorPM == 'am') {
         $('#2PM').addClass("future");
-    } else if (hour > 2 && AMorPM == 'pm'){
+    } else if (hour > 2 && AMorPM == 'pm') {
         $('#2PM').addClass("past");
     }
-    if (hour == 3 && AMorPM == 'pm'){
+    if (hour == 3 && AMorPM == 'pm') {
         $('#3PM').addClass("present");
-    } else if (hour == 12  && AMorPM == 'pm' || hour < 3 && AMorPM == 'pm' || AMorPM == 'am'){
+    } else if (hour == 12 && AMorPM == 'pm' || hour < 3 && AMorPM == 'pm' || AMorPM == 'am') {
         $('#3PM').addClass("future");
-    } else if (hour > 3 && AMorPM == 'pm'){
+    } else if (hour > 3 && AMorPM == 'pm') {
         $('#3PM').addClass("past");
     }
-    if (hour == 4 && AMorPM == 'pm'){
+    if (hour == 4 && AMorPM == 'pm') {
         $('#4PM').addClass("present");
-    } else if (hour == 12  && AMorPM == 'pm' || hour < 4 && AMorPM == 'pm' || AMorPM == 'am'){
+    } else if (hour == 12 && AMorPM == 'pm' || hour < 4 && AMorPM == 'pm' || AMorPM == 'am') {
         $('#4PM').addClass("future");
-    } else if (hour > 4 && AMorPM == 'pm'){
+    } else if (hour > 4 && AMorPM == 'pm') {
         $('#4PM').addClass("past");
-    } 
-    if (hour == 5 && AMorPM == 'pm'){
+    }
+    if (hour == 5 && AMorPM == 'pm') {
         $('#5PM').addClass("present");
-    } else if (hour == 12  && AMorPM == 'pm' || hour < 5 && AMorPM == 'pm' || AMorPM == 'am'){
+    } else if (hour == 12 && AMorPM == 'pm' || hour < 5 && AMorPM == 'pm' || AMorPM == 'am') {
         $('#5PM').addClass("future");
-    } else if (hour > 5 && AMorPM == 'pm'){
+    } else if (hour > 5 && AMorPM == 'pm') {
         $('#5PM').addClass("past");
-    }         
+    }
 }
 setInterval(clock, 1000);
 
@@ -89,41 +89,61 @@ setInterval(clock, 1000);
 
 //click on the timeblock to enter event
 
+var inputSwitch = false;
+
 $(function () {
     $(".description").bind("click", function (event) {
-        var value = event.target.id
-        console.log(event.target.children.length)
         openText(event);
     });
 });
 
 function openText(event) { //opens up the textbox inside the correct div
-    if (event.target.children.length === 0) {
-        var value = event.target.id
+    if (inputSwitch === false){
+        var value = event.target.id;
 
-        // First create a DIV element.
-        var txtNewInputBox = document.createElement('input');
+        // // First create a DIV element.
+        // var txtNewInputBox = document.createElement('input');
 
-        // Then add the content (a new input box) of the element.
-        txtNewInputBox.innerHTML = "<input type='text' id='InputBox'>";
+        // // Then add the content (a new input box) of the element.
+        // txtNewInputBox = "<input type='text' id='InputBox' name='InputBox'>";
 
-        // Finally put it where it is supposed to appear.
-        document.getElementById("" + value).appendChild(txtNewInputBox);
+        // // Finally put it where it is supposed to appear.
+        // //console.log(document.getElementById("" + value))
+        var input = document.createElement("input");
+        input.type = 'text';
+        input.id = 'InputBox';
+
+        document.getElementById("" + value).appendChild(input);
+        inputSwitch = true;
     }
 }
 
 //can click save for that timeblock
 $(function () {
     $(".saveBtn").bind("click", function (event) {
-        var value = event.target.id
-        console.log(event.target.children.length)
-        openText(event);
+        getChars(event);
     });
 });
 
-function getChars() { //Gets number of characters from textbox
-    password.characters = document.getElementById("characters").value;
-  }
+function getChars(event) { //Gets number of characters from textbox
+    var match = event.target.id;
+    match = match.slice(0, -1);
+    var descrip = document.getElementById("" + match);
+    // First create a DIV element.
+    var newP = document.createElement('li');
+    var x = document.getElementById("InputBox").value;
+    console.log(x)
+    newP.innerHTML = x;
+    // newP.innerHTML = document.getElementById("InputBox").value;
+    // console.log(document.getElementById("InputBox").)
+    // console.log(newP.innerHTML);
+    descrip.lastChild.remove();
+    myStorage = window.localStorage;
+    
+    descrip.appendChild(newP);
+    inputSwitch = false;
+
+}
 
 //text is saved in local storage
 
